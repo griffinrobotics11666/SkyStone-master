21866b1909819429f6cd7ed243d83352001016b8
sethstorino <griffinrobotics11666@gmail.com>
2020-09-17 16:26:38 -0700

    added 1st Servo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestDriveStorino.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestDriveStorino.java
@@ -33,6 +33,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
@@ -59,6 +60,127 @@ public class TestDriveStorino extends OpMode
     private DcMotor leftDrive = null;
     private DcMotor rightDrive = null;
 
+    Servo rightArmServo; //define armServo
+    Servo leftArmServo;
+    static final double INCREMENT   = 0.003;     // amount to slew servo each CYCLE_MS cycle
+    static final double MAX_POS     =  1.0;     // Maximum rotational position
+    static final double MIN_POS     =  0.0;     // Minimum rotational position
+
+
+    double  rightServoPosition = (MAX_POS - MIN_POS) / 2; // Start at halfway position
+    double  leftServoPosition = (MAX_POS - MIN_POS) / 2; // Start at halfway position
+
+
+
+    /*
+     * Code to run ONCE when the driver hits INIT
+     */
+    @Override
+    public void init() {
+        telemetry.addData("Status", "Initialized");
+
+        // Initialize the hardware variables. Note that the strings used here as parameters
+        // to 'get' must correspond to the names assigned during the robot configuration
+        // step (using the FTC Robot Controller app on the phone).
+        leftDrive  = hardwareMap.get(DcMotor.class, "left_drive");
+        rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
+        rightArmServo = hardwareMap.get(Servo.class, "right_arm_servo");
+        leftArmServo = hardwareMap.get(Servo.class, "left_arm_servo");
+
+        // Most robots need the motor on one side to be reversed to drive forward
+        // Reverse the motor that runs backwards when connected directly to the battery
+        leftDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightArmServo.setPosition(rightServoPosition);
+        leftArmServo.setPosition(leftServoPosition);
+
+        // Tell the driver that initialization is complete.
+        telemetry.addData("Status", "Initialized");
+
+
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
+     */
+    @Override
+    public void init_loop() {
+    }
+
+    /*
+     * Code to run ONCE when the driver hits PLAY
+     */
+    @Override
+    public void start() {
+        runtime.reset();
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+     */
+    @Override
+    public void loop() {
+        // Setup a variable for each drive wheel to save power level for telemetry
+        double leftPower;
+        double rightPower;
+
+
+
+
+
+
+        if (gamepad1.dpad_right){   //rotates the servo right
+            rightServoPosition += INCREMENT; //increases position
+            leftServoPosition -= INCREMENT;
+            if (rightServoPosition >= MAX_POS || leftServoPosition >= MAX_POS ) {
+                rightServoPosition = MAX_POS;
+                leftServoPosition = MAX_POS;
+            }
+            rightArmServo.setPosition(rightServoPosition);
+            leftArmServo.setPosition(leftServoPosition);
+
+
+        }
+
+        if (gamepad1.dpad_left) {
+            rightServoPosition -= INCREMENT; //increases position
+            leftServoPosition += INCREMENT;
+            if (rightServoPosition <= MIN_POS || leftServoPosition <= MIN_POS) {
+                rightServoPosition = MIN_POS;
+                leftServoPosition = MIN_POS;
+            }
+            rightArmServo.setPosition(rightServoPosition);
+            leftArmServo.setPosition(leftServoPosition);
+
+        }
+
+
+
+
+
+
+        double turn = -gamepad1.right_stick_x;
+        double drive  =  gamepad1.left_stick_y;
+
+        leftPower    = Range.clip(drive + turn, -0.5, 0.5) ;
+        rightPower   = Range.clip(drive - turn, -0.5, 0.5) ;
+
+
+        // Send calculated power to wheels
+        leftDrive.setPower(leftPower);
+        rightDrive.setPower(rightPower);
+
+        // Show the elapsed game time and wheel power.
+        telemetry.addData("Status", "Run Time: " + runtime.toString());
+        telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
+        telemetry.addData("Left servo Position", "%5.2f", leftServoPosition);
+        telemetry.addData("Right servo Position", "%5.2f", rightServoPosition);
+        telemetry.update();
+    }
+
+    /*
+     * Code to run ONCE after the driver hits STOP
+     */
     /*
      * Code to run ONCE when the driver hits INIT
      */
